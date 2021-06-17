0db81bea5e5eb1fe34ceb6f98d992e3a65b8d945
Jank Jank <jank.jank1812@gmail.com>
2021-06-17 16:10:16 -0700

    Completed classes. Adding function son main index.js now.

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+// I. requires
+// II. generateHTML
+// III. Inquirer - Manager
+// IV. Menu - add staff?
+// V. Inquierer - Engineer
+//VI. Inquierer - Intern
+
+
 const inquirer = require('inquirer');
 const fs = require('fs');
 
 const generateHTML = (answers) =>
 
-
-
-
 inquirer
+
 ///Team Manager
   .prompt([
     {
@@ -30,7 +36,10 @@ inquirer
       name: 'office',
     },
     ])
-///Engineer
+
+  ///QUESTION HERE:Would you like to add an engineer, intern, or finish?
+
+  ///Engineer
     .prompt([
         {
           type: 'input',
@@ -54,6 +63,39 @@ inquirer
         },
         ])
 
+  ///Intern
+.prompt([
+    {
+      type: 'input',
+      message: 'EInterns Name?',
+      name: 'engineer',
+    },
+    {
+      type: 'input',
+      message: 'Employee ID?',
+      name: 'eEmployeeID',
+    },
+    {
+      type: 'input',
+      message: 'Email Address?',
+      name: 'eEmailAddress',
+    },
+    {
+      type: 'input',
+      message: 'GitHub Username?',
+      name: 'eGitHUbUsername',
+    },
+    ])
+
+
+//PROMISE  (mini project 6/7 2:30)
+    .then((answers) => {
+        const htmlPageContent = generateHTML(answers);
+    
+        fs.writeFile('index.html', htmlPageContent, (err) =>
+          err ? console.log(err) : console.log('Success!You created the index.html!')
+        );
+      });
 
 
 
@@ -65,31 +107,29 @@ inquirer
 
 
 
-    // {
-    //   type: 'confirm',
-    //   message: 'Would you like to allow other developers to contribute to your project? Contributor Covenant',
-    //   name: 'contributions',
-    //   default: true,
-    // },
 
-    //  {
-    //   type: 'input',
-    //   message: 'Have you created any tests you would like to make available to the user?',
-    //   name: 'tests',
-    //   default: 'NPM',
-    // },
-    // {
-    //   name: 'license',
-    //   type: 'list',
-    //   message: 'Would you like to add a license?',
-    //   choices: [
-    //             "Apache",
-    //             "Academic",
-    //             "GNU",
-    //             "ISC",
-    //             "MIT",
-    //             "Mozilla",
-    //             "Open"
-    //   ]
-    // },
-   
+
+
+
+      // `<!DOCTYPE html>
+// <html lang="en">
+// <head>
+//   <meta charset="UTF-8">
+//   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+//   <title>Document</title>
+// </head>
+// <body>
+//   <div class="jumbotron jumbotron-fluid">
+//   <div class="container">
+//     <h1 class="display-4">Hi! My name is ${answers.name}</h1>
+//     <p class="lead">I am from ${answers.location}.</p>
+//     <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+//     <ul class="list-group">
+//       <li class="list-group-item">My GitHub username is ${answers.github}</li>
+//       <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
+//     </ul>
+//   </div>
+// </div>
+// </body>
+// </html>`;
