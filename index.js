a15150f20a04c0b11f2432b4bcd49452ae43fef0
Jank Jank <jank.jank1812@gmail.com>
2021-06-22 08:27:07 -0700

    Began adding functions

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const TeamManager=require("./lib/Manager.js");
 
-const generateHTML = (answers) =>
+//array ready to receive info we wanty t push to it.
+const teamAnswers = []
+
+function managerFunction () {
 
 inquirer
   .prompt([
@@ -25,68 +29,19 @@ inquirer
       message: 'Office #?',
       name: 'office',
     },
-    {
-      type: 'list',
-      message: 'Are there other employees you need to add?',
-      name: 'otherEmployees',
-      choices: ["Engineer", "Intern", "My team is complete."]
-    }
-  ])
-
+  ]).then((managerAnswers)=>{
+    console.log(managerAnswers)
+    const Manager = new TeamManager(managerAnswers.teamManager, managerAnswers.employeeID, managerAnswers.emailAddress, managerAnswers.office)
+    teamAnswers.push(Manager);
+    continue()
+  }
   
-
-
-//PROMISE  (mini project 6/7 2:30)
-    .then((answers) => {
-        const htmlPageContent = generateHTML(answers);
+  )
+}    
     
-        fs.writeFile('index.html', htmlPageContent, (err) =>
-          err ? console.log(err) : console.log('Success!You created the index.html!')
-        );
-      });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-`<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
-    <title>Team</title>
-  </head>
-
-
-
-  <body>
-    <div class="">
-      <h1 class="">Welcome to you the Team Builder!</h1>
-      <p class="">I am from ${answers.location}.</p>
-      <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-      <ul class="list-group">
-        <li class="list-group-item">My GitHub username is ${answers.github}</li>
-        <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
-      </ul>
-    </div>
-  </body>
-</html>`
-
-
-
-
-
+managerFunction()    
+    
+ 
 
 //   ///Engineer
 //     .prompt([
@@ -131,7 +86,64 @@ inquirer
 //     },
 //     {
 //       type: 'input',
-//       message: 'GitHub Username?',
-//       name: 'eGitHUbUsername',
+//       message: 'school?',
+//       name: 'school',
 //     },
 //     ])
+
+
+// //PROMISE  (mini project 6/7 2:30)
+//     .then((answers) => {
+//         const htmlPageContent = generateHTML(answers);
+    
+//         fs.writeFile('index.html', htmlPageContent, (err) =>
+//           err ? console.log(err) : console.log('Success!You created the index.html!')
+//         );
+//       });
+
+
+
+
+
+////////this next to ask a nd call funcs
+//       {
+//         type: 'list',
+//         message: 'Are there other employees you need to add?',
+//         name: 'otherEmployees',
+//         choices: ["Engineer", "Intern", "My team is complete."]
+//       }
+
+
+
+
+
+// const generateHTML = (answers) =>
+//   `<!DOCTYPE html>
+// <html lang="en">
+// <head>
+//   <meta charset="UTF-8">
+//   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+//   <title>Team Generator</title>
+// </head>
+
+// <body>
+//   <div class="jumbotron jumbotron-fluid">
+//   <div class="container">
+//     <h1 class="display-4">Hi! My name is ${answers.name}</h1>
+//     <p class="lead">I am from ${answers.location}.</p>
+//     <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+//     <ul class="list-group">
+//       <li class="list-group-item">My GitHub username is ${answers.github}</li>
+//       <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
+//     </ul>
+//   </div>
+// </div>
+// </body>
+// </html>`;
+
+
+
+
+
+
