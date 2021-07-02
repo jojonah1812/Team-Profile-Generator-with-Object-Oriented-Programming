bdae1d51f89416562a978be2bae85976f6d7fd7e
Jonah <82416656+jojonah1812@users.noreply.github.com>
2021-07-02 16:07:55 -0700

    Attempting to get HTML to generate - with data captured in questions.

--- index.js
+++ index.js
@@ -54,21 +54,23 @@ function nextTeamMember() {
           choices: ["Engineer", "Intern", "My team is complete."]
         }
       ])
-      .then((answers) => {
+      .then((answers) => { 
+      // console.log(answers));
         if (answers.otherEmployees === 'Engineer') {
-          engineerFunction();
+            engineerFunction();
         } else if
           (answers.otherEmployees === 'Intern') {
           internFunction();
         } else {
-            const htmlPageContent = generateHTML(answers);
+            console.log(teamAnswers);
+            const generateHTML = teamHTML(teamAnswers);
       
-            fs.writeFile('index.html', htmlPageContent, (err) =>
-              err ? console.log(err) : console.log('Success! You created the index.html!')
+            fs.writeFile('index.html', generateHTML, (err) =>
+             err ? console.log(err) : console.log('Success! You created the index.html!')
             );
         }
       
-})
+  })
 }
 
 //   ///Engineer
@@ -148,23 +150,104 @@ inquirer
 
 managerFunction ();
 
+const teamHTML = (teamAnswers) =>
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"> -->
+  <title>Team Redding</title>
+</head>
+<body> 
+    <h1>Team Redding</h1>
+    <div class="container">
+      <div class="row">
 
+          <div>
+            <h2> ${teamAnswers[0].name}</h2>
+            <h4>Team Manager</h4>
+            <ul>
+                <li> Employee ID#${teamAnswers[0].id}</li>
+                <li> Office # 9 </li>
+                <li> Gjay@redding.com </li>
+            </ul>
+          </div>
+        
+          <div>
+            <h2> Phylis Jenkins </h2>
+            <h4>Engineer</h4>
+            <ul>
+                <li> Engineer </li>
+                <li> Employee ID#298 </li>
+                <li> Pjenkins@redding.com </li>
+                <li> Jenie212 </li>
+            </ul>
+          </div>
+        
+          <div>
+            <h2> Rupret Flores </h2>
+            <h4>Intern</h4>
+            <ul>
+                <li> Employee ID# I12 </li>
+                <li> Rflores@redding.com </li>
+                <li> Georgia Tech </li>
+            </ul>
+          </div>
+        
+      </div>
+    </div>
+<body>
+</html>`
 
+// DONE WITH CHRISTIAN  ????????????????
 // //.  GENERATE HTML //
-// const generateHTML = (answers) =>
+// const teamHTML = (teamAnswers) =>
 // `<!DOCTYPE html>
-//   <html lang="en">
-//   <head>
-//       <meta charset="UTF-8">
-//       <meta http-equiv="X-UA-Compatible" content="IE=edge">
-//       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//       <title>Jonah's Team</title>
-//   </head>
-//   <body>
+// <html lang="en">
+// <head>
+//   <meta charset="UTF-8">
+//   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//   <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"> -->
+//   <title>Team Redding</title>
+// </head>
+// <body> 
+//     <h1>Team Redding</h1>
 //     <div class="container">
 //       <div class="row">
-//         ${addEmployeeCard(teamAnswers)}
+
+//           <div>
+//             <h2> ${teamAnswers[0].name}</h2>
+//             <h4>Team Manager</h4>
+//             <ul>
+//                 <li> Employee ID#${teamAnswers[0].id}</li>
+//                 <li> Office # 9 </li>
+//                 <li> Gjay@redding.com </li>
+//             </ul>
+//           </div>
+        
+//           <div>
+//             <h2> Phylis Jenkins </h2>
+//             <h4>Engineer</h4>
+//             <ul>
+//                 <li> Engineer </li>
+//                 <li> Employee ID#298 </li>
+//                 <li> Pjenkins@redding.com </li>
+//                 <li> Jenie212 </li>
+//             </ul>
+//           </div>
+        
+//           <div>
+//             <h2> Rupret Flores </h2>
+//             <h4>Intern</h4>
+//             <ul>
+//                 <li> Employee ID# I12 </li>
+//                 <li> Rflores@redding.com </li>
+//                 <li> Georgia Tech </li>
+//             </ul>
+//           </div>
+        
 //       </div>
 //     </div>
-//   </body>
-//   </html>`
+// <body>
+// </html>`
